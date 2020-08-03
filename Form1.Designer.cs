c28268c64006a9fffbf82b905326c041e9bf5056
kasusa <kasusaland@gmail.com>
2020-08-03 19:57:14 +0800

    ver 1.0 uwu

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace miniclock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.hacker_styleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.biggersizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.positionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topLeftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topRightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(3, 1);
+            this.label1.MinimumSize = new System.Drawing.Size(50, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "12:08";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.hacker_styleToolStripMenuItem,
+            this.positionToolStripMenuItem,
+            this.biggersizeToolStripMenuItem,
+            this.aDToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(186, 152);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(185, 24);
+            this.toolStripMenuItem1.Text = "random theme";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // hacker_styleToolStripMenuItem
+            // 
+            this.hacker_styleToolStripMenuItem.BackColor = System.Drawing.SystemColors.Info;
+            this.hacker_styleToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.hacker_styleToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.AliceBlue;
+            this.hacker_styleToolStripMenuItem.Name = "hacker_styleToolStripMenuItem";
+            this.hacker_styleToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.hacker_styleToolStripMenuItem.Text = "hacker_style";
+            this.hacker_styleToolStripMenuItem.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.hacker_styleToolStripMenuItem.Click += new System.EventHandler(this.hacker_styleToolStripMenuItem_Click);
+            // 
+            // biggersizeToolStripMenuItem
+            // 
+            this.biggersizeToolStripMenuItem.ForeColor = System.Drawing.Color.Yellow;
+            this.biggersizeToolStripMenuItem.Name = "biggersizeToolStripMenuItem";
+            this.biggersizeToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.biggersizeToolStripMenuItem.Text = "help";
+            this.biggersizeToolStripMenuItem.Click += new System.EventHandler(this.biggersizeToolStripMenuItem_Click);
+            // 
+            // positionToolStripMenuItem
+            // 
+            this.positionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.topLeftToolStripMenuItem,
+            this.topRightToolStripMenuItem});
+            this.positionToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.positionToolStripMenuItem.Name = "positionToolStripMenuItem";
+            this.positionToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.positionToolStripMenuItem.Text = "position";
+            // 
+            // topLeftToolStripMenuItem
+            // 
+            this.topLeftToolStripMenuItem.Name = "topLeftToolStripMenuItem";
+            this.topLeftToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.topLeftToolStripMenuItem.Text = "top left";
+            this.topLeftToolStripMenuItem.Click += new System.EventHandler(this.topLeftToolStripMenuItem_Click);
+            // 
+            // topRightToolStripMenuItem
+            // 
+            this.topRightToolStripMenuItem.Name = "topRightToolStripMenuItem";
+            this.topRightToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.topRightToolStripMenuItem.Text = "top right";
+            this.topRightToolStripMenuItem.Click += new System.EventHandler(this.topRightToolStripMenuItem_Click);
+            // 
+            // aDToolStripMenuItem
+            // 
+            this.aDToolStripMenuItem.ForeColor = System.Drawing.Color.Crimson;
+            this.aDToolStripMenuItem.Name = "aDToolStripMenuItem";
+            this.aDToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.aDToolStripMenuItem.Text = "close";
+            this.aDToolStripMenuItem.Click += new System.EventHandler(this.aDToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(470, 100);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "overwatch timebar";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveBorder;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem hacker_styleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem positionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topLeftToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topRightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem biggersizeToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miniclock
+{
+    public partial class Form1 : Form
+    {
+        Color defbackground;
+        int mactype_w = 50; //这个变量是用来治疗mactype导致自己渲染之后显示不全的问题的
+        public Form1()
+        {
+            this.ShowInTaskbar = false;//这个程序将不会显示在任务栏
+            InitializeComponent();
+            defbackground = label1.BackColor; //保存一下初始的那个背景颜色，因为打字打不出来
+
+            timer1.Interval = 10000; //10s one tick timer
+            timer1.Tick += new EventHandler(gettime); //ontick event
+            timer1.Start();
+            label1.Text = System.DateTime.Now.ToShortTimeString(); // init time
+
+
+            this.Width = 55;
+            this.Height = 18;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Right - mactype_w, 0);//显示在右上角
+        }
+
+        private void gettime(Object myObject,EventArgs myEventArgs)
+        {
+            DateTime now = System.DateTime.Now;
+            label1.Text = now.ToShortTimeString();
+            //label1.Text = now.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            contextMenuStrip1.Show(Cursor.Position);//在鼠标位置显示一个菜单
+        }
+
+        //菜单中的关闭按钮
+        private void aDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //更换主题,实际上就是从下面的数组里面,随机一个颜色换到字符上
+            //http://www.flounder.com/csharp_color_table.htm
+            Color foreColorNow = label1.ForeColor;
+            Color[] colors = 
+                { Color.Aqua
+                    , Color.PowderBlue
+                    , Color.Plum
+                    , Color.Gold
+                    , Color.DarkOrange
+                    , Color.Chartreuse
+            };
+            ;
+            label1.ForeColor = colors[new Random().Next(0, colors.Length)];
+            //随机的时候使用的范围是0~color的长度
+
+            if (label1.ForeColor==foreColorNow)//为了保证使用的体验，如果随机到了和当前同样的颜色，要把这个颜色改掉
+                label1.ForeColor = colors[new Random().Next(0, colors.Length)];
+
+        }
+
+        private void hacker_styleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //我称之为骇客模式,其实就是把背景颜色变成纯黑
+            if (label1.BackColor==defbackground)
+                label1.BackColor = Color.Black;
+            else
+                label1.BackColor = defbackground;
+
+        }
+
+        private void topLeftToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Location = new Point(0, 0);//显示在zuo上角
+
+        }
+
+        private void topRightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Right - mactype_w, 0);//显示在右上角
+
+        }
+
+        private void biggersizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("" +
+                "假如时间显示不全，请调整你的DPI设置\n"+
+                "属性-兼容性-更改高dpI设置-高DPI缩放执行-应用程序\n" +
+                "然后重新启动应用\n---------\n" +
+                "If the time display is incomplete, adjust your DPI Settings \n" +
+                "Attribute - compatibility - change high dpI Settings - high dpI zoom ,select application \n" +
+                "And then restart the application.");
+        }
+    }
+}
